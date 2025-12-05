4917a7a23fa20754d3c770727cd381422898b3fa
notshriyansh <shriyansh707@gmail.com>
2025-12-05 21:55:00 +0530

    Updated body.tsx

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,7 +1,8 @@
 import RestaurantCard from "./RestaurantCard";
 import { useState, useEffect } from "react";
 import Shimmer from "./Shimmer";
-import { Restaurant, ApiResponse, ApiRestaurantResponse } from "../utils/types";
+import { Restaurant, ApiResponse } from "../utils/types";
+import useOnlineStatus from "../utils/useOnlineStatus";
 
 const Body = () => {
   const [listOfRestaurants, setListOfRestaurants] = useState<Restaurant[]>([]);
@@ -21,6 +22,10 @@ const Body = () => {
 
     const json: ApiResponse = await data.json();
 
+    const restaurantCard = json?.data?.cards?.find(
+      (card: any) => card?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+
     const restaurants =
       json?.data?.cards?.[4]?.card?.card?.gridElements?.infoWithStyle
         ?.restaurants || [];
@@ -28,6 +33,14 @@ const Body = () => {
     setListOfRestaurants(restaurants.map((r) => r.info));
     setFilteredRestaurants(restaurants.map((r) => r.info));
   };
+
+  const onlineStatus = useOnlineStatus();
+
+  if (onlineStatus == false) {
+    return (
+      <h1>Looks like you're offline! Please check your internet connection.</h1>
+    );
+  }
 
   return listOfRestaurants.length === 0 ? (
     <Shimmer />
